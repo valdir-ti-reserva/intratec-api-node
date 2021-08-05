7ad0525c07ef4e6e55d1d3de4940e4363e7bea68
Valdir Silva <v.souza.da.silva@avanade.com>
2021-08-04 21:33:47 -0300

    feat(database): create database

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
+import 'reflect-metadata';
 import express from 'express';
+
+import './database';
 
 const app = express();
 const PORT = 3000;
 
 app.get('/', (_, res) => {
   return res.send('API Intratec!')
-})
+});
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
