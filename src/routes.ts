47ec370ab4bcf414bf8409c0e478c71b3e059c4f
Valdir Silva <v.souza.da.silva@avanade.com>
2021-09-07 19:56:39 -0300

    feat(api): authenticate user

--- src/routes.ts
+++ src/routes.ts
@@ -1,22 +1,25 @@
 import { Router } from 'express'
 
-import { CreateUserController } from './controllers/CreateUserController'
 import { CreateTagController } from './controllers/CreateTagController'
+import { CreateUserController } from './controllers/CreateUserController'
+import { AuthenticateUserController } from './controllers/AuthenticateUserController'
 
 import { ensureAdmin } from './middlewares/ensureAdmin'
 
 const router = Router()
 
-const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
+const createUserController = new CreateUserController()
+const authenticateUserController = new AuthenticateUserController()
 
 router.get('/', (_, res) => {
   return res.send('API ok!')
 })
 
-router.post('/users', createUserController.handle) 
+router.post('/users', createUserController.handle)
+router.post('/login', authenticateUserController.handle)
 
 router.use(ensureAdmin)
-router.post('/tags', createTagController.handle)
+router.post('/tags', createTagController.handle) 
 
 export { router }
