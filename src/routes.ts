863c16e211af0ce456b5e44bda81efca9d62cddc
Valdir Silva <v.souza.da.silva@avanade.com>
2021-09-08 21:32:11 -0300

    refact(compliments): move routes to a new folder

--- src/routes.ts
+++ src/routes.ts
@@ -2,18 +2,12 @@ import { Router } from 'express'
 
 import { routes } from './routes/index'
 
-import { CreateComplimentController } from './controllers/CreateComplimentsController'
-
 const router = Router()
-
-const createComplimentController = new CreateComplimentController()
 
 router.get('/', (_, res) => {
   return res.send('API ok!')
 })
 
 router.use(routes)
-
-router.post('/compliments', createComplimentController.handle)
 
 export { router }
